4f2645e169fc104f1036d419a0d4ca9ba10284eb
normadiaz <normadiazchb@me.com>
2022-01-28 10:08:46 -0600

    history city stop 10, UV data complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,16 +34,41 @@ function getApi() {
             Humidity: ${data.main.humidity}
             <br>
             <br><br>
-            UV Index:<div id="uv"></div>
+            UV Index: <spam id="uv"></spam>
             <br>
             <br>`;
             document.getElementById("citydata").innerHTML = weather;
+            // Current Day
+            var date = new Date();
+            var day = (new Intl.DateTimeFormat('en-US', {
+                month: 'numeric',
+                day: '2-digit',
+                year: 'numeric',
+            }).format(date));
+            document.getElementById("currentcity").innerHTML = data.name + " (" + day + ")";
+
+            var onecall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + data.coord.lat + "&lon=" + data.coord.lon + "&appid=e14c3208e3e6754ae41625a043fd64c7&units=imperial";
+
+            fetch(onecall)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+
+                    console.log(data);
+                    document.getElementById("uv").innerHTML = data.current.uvi;
+                });
 
         });
 }
 searchButton.addEventListener('click', getApi);
 function addHistory(city) {
     searchHistory.unshift(city);
+    if (searchHistory.length > 10) {
+        searchHistory.pop()
+        var parent = document.getElementsByClassName("historycontainer")[0];
+        parent.removeChild(parent.lastChild);
+    }
     var el = document.createElement("div");
     el.classList.add("history");
     el.innerText = city;
